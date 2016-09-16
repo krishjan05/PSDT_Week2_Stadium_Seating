d4969b6a4ed0e59ea242356e8c7868440ae0cd9a
Oleksandr Rudavka <Orudavka9183@conestogac.on.ca>
2016-09-16 09:55:27 -0400

    Add calculations

--- Stadium Seating/Stadium Seating/MainWindow.xaml.cs
+++ Stadium Seating/Stadium Seating/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Stadium_Seating
 {
     /// <summary>
@@ -20,6 +21,10 @@ namespace Stadium_Seating
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int classAPrice = 15;
+        private int classBPrice = 12;
+        private int classCPrice = 9;
+
         private TextBox currentBox;
         public MainWindow()
         {
@@ -63,6 +68,59 @@ namespace Stadium_Seating
         private void gotFocus(object sender, RoutedEventArgs e)
         {
             currentBox = (TextBox)sender;
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            prepareTextBoxesToParse();
+            int aClassTickets;
+            int bClassTickets;
+            int cClassTickets;
+            if (int.TryParse(classASoldBox.Text, out aClassTickets) &&
+                int.TryParse(classBSoldBox.Text, out bClassTickets) &&
+                int.TryParse(classCSoldBox.Text, out cClassTickets))
+            {
+                int totalAPrice = aClassTickets * classAPrice;
+                int totalBPrice = bClassTickets * classBPrice;
+                int totalCPrice = cClassTickets * classCPrice;
+                int totalPrice = totalAPrice + totalBPrice + totalCPrice;
+
+                
+
+                string output = "Price: \n" +
+                    "\nA Class:      $" + spacesforNumber(totalAPrice) + totalAPrice + ".00" +
+                    "\nB Class:      $" + totalBPrice + ".00" +
+                    "\nC Class:      $" + totalCPrice + ".00" +
+                    "\nTotal price: $"  + totalPrice  + ".00";
+                displayLabel.Content = output;
+            } else
+            {
+                MessageBox.Show("Wrong input! You can only use integer numbers without floating point");
+
+            }
+        }
+
+        private void prepareTextBoxesToParse()
+        {
+            TextBox[] textBoxes = new TextBox[] { classASoldBox, classBSoldBox, classCSoldBox };
+            foreach (TextBox textBox in textBoxes) {
+                if (textBox.Text.Length < 1)
+                {
+                    textBox.Text = "0";
+                }
+            }
+        }
+
+        private string spacesforNumber(int number)
+        {
+            int exponent = 0;
+            string spaces = "";
+            for (int total = number; total > 0; total /= 10)
+            {
+                exponent++;
+                spaces = spaces + " ";
+            }
+            return spaces;
         }
     }
 }
